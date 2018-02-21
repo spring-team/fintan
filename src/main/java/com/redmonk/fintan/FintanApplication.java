fc68b75382cd090339b97762cad3eb88d9a2d7b7
Rod Johnson <johnsonroda@gmail.com>
2018-02-21 10:35:21 -0800

    Initial commit from Atomist

---
+++ src/main/java/com/redmonk/fintan/FintanApplication.java
@@ -0,0 +1,12 @@
+package com.redmonk.fintan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintanApplication.class, args);
+	}
+}
